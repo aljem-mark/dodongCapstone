73b874903f60780f32cec6e84a9d85592c81ec15
aljem-mark <speakout.aljem@gmail.com>
2019-06-09 16:42:55 +0800

    update apikeys

--- config.php
+++ config.php
@@ -9,6 +9,16 @@
 	$con = mysqli_connect($server,$user,$pass);
 	mysqli_select_db($con, $dbname);
 	
+	// Aljem
 	$smsApiKey = urlencode('D7AQFNNY9Sk-yKXwrma3PCZHAsFCqUctyUcto9ccnY');
+
+	// Dodong
+	// $smsApiKey = urlencode('D7AQFNNY9Sk-yKXwrma3PCZHAsFCqUctyUcto9ccnY');
+
+	// Gogong
+	// $smsApiKey = urlencode('D7AQFNNY9Sk-yKXwrma3PCZHAsFCqUctyUcto9ccnY');
+
+	// Rontoy
+	// $smsApiKey = urlencode('D7AQFNNY9Sk-yKXwrma3PCZHAsFCqUctyUcto9ccnY');
 
 ?>
